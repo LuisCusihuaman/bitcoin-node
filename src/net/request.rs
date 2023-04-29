49e359211cf2c1516945831124d44fdacf586e31
Nox <davela@fi.uba.ar>
2023-04-29 19:27:02 -0300

    wip: request
    Co-authored-by: acmaiolo <amaiolo@fi.uba.ar>
    Co-authored-by: LuisCusihuaman <lcusihuaman@fi.uba.ar>
    Co-authored-by: LuisParedes1 <lparedesr@fi.uba.ar>

--- src/net/request.rs
+++ src/net/request.rs
@@ -1,13 +1,56 @@
-pub struct Request {}
+use std::collections::HashMap;
+use std::io::{BufRead, BufReader};
+use std::net::TcpStream;
+pub struct Request {
+    path: String,
+    params: HashMap<String, String>,
+}
 
 impl Request {
-    pub fn new() -> Request {
-        Request {}
-    }
+    pub fn parse(stream: &mut TcpStream) -> Request {
+        let mut lines = String::new();
+        let mut reader = BufReader::new(stream);
+        let _ = reader.read_line(&mut lines);
+        let mut line = lines.split_whitespace();
 
-    pub fn json(s: &str) -> String {
-        let mut s = String::from(s);
-        s.push_str("json");
-        s
+        let path = match line.nth(0) {
+            Some(e) => e,
+            None => "/",
+        };
+
+        let path_querys: Vec<&str> = path.split("?").collect();
+        let req_path = path_querys[0]; //?a=1
+        let query_string = match path_querys.get(1).or(None) {
+            Some(q) => {
+                let tags: HashMap<String, String> = q
+                    .split('&')
+                    .map(|kv| kv.split('=').collect::<Vec<&str>>())
+                    .map(|vec| {
+                        assert_eq!(vec.len(), 2);
+                        (vec[0].to_string(), vec[1].to_string())
+                    })
+                    .collect();
+                tags
+            }
+            None => HashMap::new(),
+        };
+
+        Request {
+            path: req_path.to_string(),
+            params: query_string,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_parse() {
+        //let mut streamMockFromString = Buffer::new();
+        //streamMockFromString.write(b"GET /get HTTP/1.1\r\n\r\n");
+        //let req = Request::parse(&mut streamMockFromString);
+        //assert_eq!(req.path, "/get");
+        //assert_eq!(req.params.len(), 0);
     }
 }

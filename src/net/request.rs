40c55d3cfd6f9bf2c56f722f7094d324e8ebe874
Nox <davela@fi.uba.ar>
2023-04-30 13:30:11 -0300

    feat: request Co-authored-by: LuisCusihuaman <lcusihuaman@fi.uba.ar>

--- src/net/request.rs
+++ src/net/request.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Read, Write};
 use std::net::TcpStream;
+
 pub struct Request {
     path: String,
     params: HashMap<String, String>,
@@ -8,49 +9,84 @@ pub struct Request {
 
 impl Request {
     pub fn parse(stream: &mut TcpStream) -> Request {
-        let mut lines = String::new();
-        let mut reader = BufReader::new(stream);
-        let _ = reader.read_line(&mut lines);
-        let mut line = lines.split_whitespace();
+        parse_internal(stream)
+    }
+}
 
-        let path = match line.nth(0) {
-            Some(e) => e,
-            None => "/",
-        };
+fn parse_internal<T: Read + Write>(stream: T) -> Request {
+    let mut lines = String::new();
+    let mut reader = BufReader::new(stream);
+    let _ = reader.read_line(&mut lines);
+    let mut line = lines.split_whitespace();
 
-        let path_querys: Vec<&str> = path.split("?").collect();
-        let req_path = path_querys[0]; //?a=1
-        let query_string = match path_querys.get(1).or(None) {
-            Some(q) => {
-                let tags: HashMap<String, String> = q
-                    .split('&')
-                    .map(|kv| kv.split('=').collect::<Vec<&str>>())
-                    .map(|vec| {
-                        assert_eq!(vec.len(), 2);
-                        (vec[0].to_string(), vec[1].to_string())
-                    })
-                    .collect();
-                tags
-            }
-            None => HashMap::new(),
-        };
+    let path = match line.nth(0) {
+        Some(e) => e,
+        None => "/",
+    };
 
-        Request {
-            path: req_path.to_string(),
-            params: query_string,
+    let path_querys: Vec<&str> = path.split("?").collect();
+    let req_path = path_querys[0]; //?a=1
+    let query_string = match path_querys.get(1).or(None) {
+        Some(q) => {
+            let tags: HashMap<String, String> = q
+                .split('&')
+                .map(|kv| kv.split('=').collect::<Vec<&str>>())
+                .map(|vec| {
+                    assert_eq!(vec.len(), 2);
+                    (vec[0].to_string(), vec[1].to_string())
+                })
+                .collect();
+            tags
         }
+        None => HashMap::new(),
+    };
+
+    Request {
+        path: req_path.to_string(),
+        params: query_string,
     }
 }
 
 #[cfg(test)]
 mod tests {
+
     use super::*;
+    use std::io;
+
+    /// MockTcpStream es una mock que implementa los traits Read y Write, los mismos que implementa el TcpStream
+    struct MockTcpStream {
+        read_data: Vec<u8>,
+        write_data: Vec<u8>,
+    }
+
+    impl Read for MockTcpStream {
+        /// Lee bytes del stream hasta completar el buffer y devuelve cuantos bytes fueron leidos
+        fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+            self.read_data.as_slice().read(buf)
+        }
+    }
+
+    impl Write for MockTcpStream {
+        /// Escribe el valor del buffer en el stream y devuelve cuantos bytes fueron escritos
+        fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+            self.write_data.write(buf)
+        }
+
+        fn flush(&mut self) -> io::Result<()> {
+            self.write_data.flush()
+        }
+    }
+
     #[test]
     fn test_parse() {
-        //let mut streamMockFromString = Buffer::new();
-        //streamMockFromString.write(b"GET /get HTTP/1.1\r\n\r\n");
-        //let req = Request::parse(&mut streamMockFromString);
-        //assert_eq!(req.path, "/get");
-        //assert_eq!(req.params.len(), 0);
+        let mut mock = MockTcpStream {
+            read_data: "/get?name=pepito\n".as_bytes().to_vec(),
+            write_data: Vec::new(),
+        };
+
+        let req = parse_internal(&mut mock);
+
+        assert_eq!(req.path, "/get");
+        assert_eq!(req.params.len(), 1);
     }
 }

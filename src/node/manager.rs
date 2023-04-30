beb8511e0c54523ffbc3326059914d23f5df1e5f
Nox <davela@fi.uba.ar>
2023-04-30 16:50:02 -0300

    feat(node): download from DNS
    Co-authored-by: LuisCusihuaman <lcusihuaman@fi.uba.ar>

---
+++ src/node/manager.rs
@@ -0,0 +1,63 @@
+use std::net::{IpAddr, ToSocketAddrs};
+
+pub struct NodeManager {
+    node_network: Vec<IpAddr>,
+    config: Config,
+}
+
+pub struct Config {
+    addrs: String,
+    port: u16,
+}
+
+impl NodeManager {
+    pub fn new(config: Config) -> NodeManager {
+        NodeManager {
+            config,
+            node_network: Vec::new(),
+        }
+    }
+    fn resolve_hostname(&self, hostname: &str, port: u16) -> Result<Vec<IpAddr>, std::io::Error> {
+        // resolve the hostname to a list of SocketAddr objects
+        let addrs = (hostname, port).to_socket_addrs()?;
+
+        // extract the IP addresses from the SocketAddr objects
+        let ips: Vec<IpAddr> = addrs
+            .filter_map(|addr| match addr.ip() {
+                IpAddr::V4(ipv4) => Some(IpAddr::V4(ipv4)),
+                IpAddr::V6(_) => None,
+            })
+            .collect();
+
+        if ips.is_empty() {
+            Err(std::io::Error::new(
+                std::io::ErrorKind::AddrNotAvailable,
+                "could not resolve hostname",
+            ))
+        } else {
+            Ok(ips)
+        }
+    }
+    pub fn load_initial_nodes(&mut self) -> Result<(), std::io::Error> {
+        let ips = self.resolve_hostname(&self.config.addrs, self.config.port)?;
+        self.node_network
+            .extend(ips.into_iter().filter(|addr| addr.is_ipv4()));
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_get_all_ips_from_dns() {
+        let mut node_manager = NodeManager::new(Config {
+            addrs: "seed.testnet.bitcoin.sprovoost.nl".to_string(),
+            port: 80,
+        });
+        node_manager.load_initial_nodes().unwrap();
+        assert_ne!(node_manager.node_network.len(), 0);
+    }
+}

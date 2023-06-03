13827fa0d1e67cfbb77d3da6264068d0edabe844
Nox <davela@fi.uba.ar>
2023-06-03 16:40:01 -0300

    delete net modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 pub mod config;
 pub mod error;
 pub mod logger;
-pub mod net;
 pub mod node;
 pub mod utils;
--- src/node/mod.rs
+++ src/node/mod.rs
@@ -1,5 +1,3 @@
-pub mod handler;
 pub mod manager;
 pub mod message;
 mod p2p_connection;
-//mod main
